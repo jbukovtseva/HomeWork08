c4262d967a9a3ccf24026e5264587bfc6372b4c1
Жанна Буковцева <jbukovtseva@mail.ru>
2022-09-03 15:14:21 +0300

    Trask1

---
+++ Trask1/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] randomArray = GetFillArray(3, 5);
+PrintArray(randomArray);
+Console.WriteLine();
+int[,] sortArray = SortDescendingOrder(randomArray);
+PrintArray(sortArray);
+
+
+int[,] GetFillArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random randomElement = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = randomElement.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortDescendingOrder(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                int max = array[0, 0];
+                int temp = 0;
+                if (array[i, j] < array[i, j + 1])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
